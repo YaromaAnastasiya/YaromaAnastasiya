59f480ef3268c5f787b1b4b6428609a15da38341
Alexander Mizhgorodskiy <yaroma.anastasiya@gmail.com>
2022-12-09 21:09:11 +0300

    add

---
+++ YaromaAnastasiya2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YaromaAnastasiya2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+            if (name == "Вячеслав")
+            {
+                Console.WriteLine("Привет Вячеслав");
+            }
+            else
+            {
+                Console.WriteLine("нет такого имени");
+            }
+            Console.ReadLine();
+        }
+    }
+}
