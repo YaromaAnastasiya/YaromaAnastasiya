ad993af3009756b2aaf7edd7ff6267b73ade84d4
Alexander Mizhgorodskiy <yaroma.anastasiya@gmail.com>
2022-12-09 22:06:06 +0300

    add

---
+++ YaromaAnastasiya3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YaromaAnastasiya3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int element = int.Parse(Console.ReadLine());
+            int[]myArray = new int[element];
+            
+
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i <myArray.Length; i++)
+            {
+                if (myArray[i]%3 == 0)
+                {
+                    Console.WriteLine(myArray[i]);
+                }
+                
+            }
+            Console.ReadLine();
+            
+
+
+        }
+    }
+}
