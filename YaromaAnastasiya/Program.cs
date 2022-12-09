335ad559a25d2c035e030691686115a1c75c8b88
Alexander Mizhgorodskiy <yaroma.anastasiya@gmail.com>
2022-12-09 21:02:33 +0300

    Добавьте файлы проекта.

---
+++ YaromaAnastasiya/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YaromaAnastasiya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            string number = Console.ReadLine();
+            int number_1 = Convert.ToInt32(number);
+            if (number_1 > 7)
+            {
+
+                Console.WriteLine("привет");
+            }
+        }
+    }
+}
